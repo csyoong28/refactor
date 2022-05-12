d1b263adeb25b5392c730f257a9b24e231ab1ef5
soonyoong.chia <soonyoong.chia@hytechc.com>
2022-05-12 10:49:36 +0800

    add

--- src/main/java/com/refactoring/refactor/App.java
+++ src/main/java/com/refactoring/refactor/App.java
@@ -6,8 +6,41 @@ package com.refactoring.refactor;
  */
 public class App 
 {
+    String name;
+    
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        App app = new App();
+        app.name= "john";
+        app.T01_extractMethod_dirty();
     }
+    
+    public void T01_extractMethod_dirty() {
+        printBanner();
+
+        // Print details.
+        System.out.println("name: " + name);
+        System.out.println("amount: " + getOutstanding());
+    }
+    
+    public void T01_extractMethod_solution() {
+        printBanner();
+        // Print details.
+        printDetails();
+    }
+
+    public void printDetails() {
+        System.out.println("name: " + name);
+        System.out.println("amount: " + getOutstanding());
+    }
+    
+    public void printBanner() {
+        System.out.println("printBanner() run..");
+    }
+    
+    public double getOutstanding() {
+        return 100.01;
+    }
+    
+    
 }
